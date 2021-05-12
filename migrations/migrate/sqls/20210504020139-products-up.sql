a7799964ca06603e06621f3f4211aa84c334fb78
rzlams <lamsarmiento@gmail.com>
2021-05-11 22:29:37 -0400

    remove BLOCKED status on products

--- migrations/migrate/sqls/20210504020139-products-up.sql
+++ migrations/migrate/sqls/20210504020139-products-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `products` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `product_type` ENUM('SERVICE','EQUIPMENT','PART') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `status` ENUM('ACTIVE','INACTIVE','BLOCKED') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'ACTIVE' NOT NULL,
+  `status` ENUM('ACTIVE','INACTIVE') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'ACTIVE' NOT NULL,
   `name` VARCHAR(100) NOT NULL,
   `code` VARCHAR(50) DEFAULT NULL,
   `serial_number` VARCHAR(50) DEFAULT NULL,
@@ -12,7 +12,10 @@ CREATE TABLE `products` (
   `block_reason` TEXT DEFAULT NULL,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_by` INT NOT NULL,
+  `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT NULL,
+  `updated_by` INT,
   CONSTRAINT products_pk PRIMARY KEY (`id`),
   CONSTRAINT products_created_by_fk FOREIGN KEY (created_by) REFERENCES users(id),
+  CONSTRAINT products_updated_by_fk FOREIGN KEY (updated_by) REFERENCES users(id),
   CONSTRAINT products_block_reason_check CHECK (status <> 'BLOCKED' OR block_reason IS NOT NULL)
 ) ENGINE=InnoDB;
