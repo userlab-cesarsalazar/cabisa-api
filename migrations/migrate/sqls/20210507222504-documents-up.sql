d9729e12fe81feeba69a83793902f33f7e19d2fd
rzlams <lamsarmiento@gmail.com>
2021-05-15 18:23:46 -0400

    add foreign key to related_internal_document_id

--- migrations/migrate/sqls/20210507222504-documents-up.sql
+++ migrations/migrate/sqls/20210507222504-documents-up.sql
@@ -3,7 +3,8 @@ CREATE TABLE `documents` (
   `document_type` ENUM('SELL_INVOICE','RENT_INVOICE','SELL_PRE_INVOICE','PURCHASE_ORDER','RENT_PRE_INVOICE') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `stakeholder_id` INT NOT NULL,
   `operation_id` INT,
-  `related_document_id` INT,
+  `related_internal_document_id` INT DEFAULT NULL,
+  `related_external_document_id` VARCHAR(50) DEFAULT NULL,
   `status` ENUM('PENDING','APPROVED','CANCELLED') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'PENDING' NOT NULL,
   `start_date` TIMESTAMP,
   `end_date` TIMESTAMP,
@@ -13,15 +14,15 @@ CREATE TABLE `documents` (
   `authorized_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `authorized_by` INT,
   CONSTRAINT documents_pk PRIMARY KEY (id),
-  CONSTRAINT documents_products_stakeholder_id_fk FOREIGN KEY (stakeholder_id) REFERENCES stakeholders(id),
-  CONSTRAINT documents_products_operation_id_fk FOREIGN KEY (operation_id) REFERENCES operations(id),
-  CONSTRAINT documents_products_created_by_fk FOREIGN KEY (created_by) REFERENCES users(id),
-  CONSTRAINT documents_products_authorized_by_fk FOREIGN KEY (authorized_by) REFERENCES users(id),
+  CONSTRAINT documents_stakeholder_id_fk FOREIGN KEY (stakeholder_id) REFERENCES stakeholders(id),
+  CONSTRAINT documents_operation_id_fk FOREIGN KEY (operation_id) REFERENCES operations(id),
+  CONSTRAINT documents_related_internal_document_id_fk FOREIGN KEY (related_internal_document_id) REFERENCES documents(id) MATCH PARTIAL ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT documents_created_by_fk FOREIGN KEY (created_by) REFERENCES users(id),
+  CONSTRAINT documents_authorized_by_fk FOREIGN KEY (authorized_by) REFERENCES users(id),
   CONSTRAINT documents_operation_id_check CHECK (status <> 'APPROVED' OR operation_id IS NOT NULL),
-  CONSTRAINT documents_related_document_id_check CHECK (operation_id IS NULL OR related_document_id IS NOT NULL),
   CONSTRAINT documents_start_date_check CHECK ((document_type <> 'RENT' AND document_type <> 'RENT_PRE_INVOICE') OR start_date IS NOT NULL),
   CONSTRAINT documents_end_date_check CHECK ((document_type <> 'RENT' AND document_type <> 'RENT_PRE_INVOICE') OR end_date IS NOT NULL),
-  CONSTRAINT documents_authorized_by_check CHECK (status = 'PENDING' OR authorized_by IS NOT NULL)
+  CONSTRAINT documents_authorized_by_check CHECK (status = 'PENDING' OR authorized_by IS NOT NULL)    
 ) ENGINE=InnoDB;
 
 CREATE TABLE `documents_products` (
--- migrations/seed/sqls/20210509004916-documents-up.sql
+++ migrations/seed/sqls/20210509004916-documents-up.sql
@@ -1,11 +1,11 @@
-INSERT INTO documents (document_type, stakeholder_id, operation_id, related_document_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('PURCHASE_ORDER', 10, 1, 12345, 'APPROVED', '2020-07-15 09:45:13', 3, '2020-07-15 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, operation_id, related_external_document_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('PURCHASE_ORDER', 10, 1, 12345, 'APPROVED', '2020-07-15 09:45:13', 3, '2020-07-15 09:45:13', 1);
 INSERT INTO documents (document_type, stakeholder_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('SELL_PRE_INVOICE', 5, 'PENDING', '2020-08-15 09:45:13', 2, '2020-08-15 09:45:13', 1);
-INSERT INTO documents (document_type, stakeholder_id, operation_id, related_document_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('SELL_INVOICE', 5, 2, 2, 'APPROVED', '2020-09-01 09:45:13', 1, '2020-09-01 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, operation_id, related_internal_document_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('SELL_INVOICE', 5, 2, 2, 'APPROVED', '2020-09-01 09:45:13', 1, '2020-09-01 09:45:13', 1);
 INSERT INTO documents (document_type, stakeholder_id, status, start_date, end_date, created_at, created_by, authorized_at, authorized_by) VALUES ('RENT_PRE_INVOICE', 6, 'PENDING', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-20 09:45:13', 2, '2021-01-24 09:45:13', 1);
-INSERT INTO documents (document_type, stakeholder_id, operation_id, related_document_id, status, start_date, end_date, created_at, created_by, authorized_at, authorized_by) VALUES ('RENT_INVOICE', 6, 3, 4, 'APPROVED', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-24 09:45:13', 1, '2021-01-24 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, operation_id, related_internal_document_id, status, start_date, end_date, created_at, created_by, authorized_at, authorized_by) VALUES ('RENT_INVOICE', 6, 3, 4, 'APPROVED', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-24 09:45:13', 1, '2021-01-24 09:45:13', 1);
 
-UPDATE documents SET status = 'APPROVED', operation_id = 2, related_document_id = 2 WHERE id = 3;
-UPDATE documents SET status = 'APPROVED', operation_id = 3, related_document_id = 4 WHERE id = 5;
+UPDATE documents SET status = 'APPROVED', operation_id = 2, related_internal_document_id = 2 WHERE id = 3;
+UPDATE documents SET status = 'APPROVED', operation_id = 3, related_internal_document_id = 4 WHERE id = 5;
 
 INSERT INTO documents_products (document_id, product_id, product_price, product_quantity) VALUES (1, 5, 35, 5);
 INSERT INTO documents_products (document_id, product_id, product_price, product_quantity) VALUES (1, 6, 15, 5);
