9e887b5117a281f8232f64b98b94719da5a2b71b
rzlams <lamsarmiento@gmail.com>
2021-05-20 12:28:30 -0400

    add projects

--- migrations/seed/sqls/20210509004916-documents-up.sql
+++ migrations/seed/sqls/20210509004916-documents-up.sql
@@ -1,8 +1,11 @@
-INSERT INTO documents (document_type, stakeholder_id, operation_id, related_external_document_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('PURCHASE_ORDER', 10, 1, 12345, 'APPROVED', '2020-07-15 09:45:13', 3, '2020-07-15 09:45:13', 1);
-INSERT INTO documents (document_type, stakeholder_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('SELL_PRE_INVOICE', 5, 'PENDING', '2020-08-15 09:45:13', 2, '2020-08-15 09:45:13', 1);
-INSERT INTO documents (document_type, stakeholder_id, operation_id, related_internal_document_id, status, created_at, created_by, authorized_at, authorized_by) VALUES ('SELL_INVOICE', 5, 2, 2, 'APPROVED', '2020-09-01 09:45:13', 1, '2020-09-01 09:45:13', 1);
-INSERT INTO documents (document_type, stakeholder_id, status, start_date, end_date, created_at, created_by, authorized_at, authorized_by) VALUES ('RENT_PRE_INVOICE', 6, 'PENDING', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-20 09:45:13', 2, '2021-01-24 09:45:13', 1);
-INSERT INTO documents (document_type, stakeholder_id, operation_id, related_internal_document_id, status, start_date, end_date, created_at, created_by, authorized_at, authorized_by) VALUES ('RENT_INVOICE', 6, 3, 4, 'APPROVED', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-24 09:45:13', 1, '2021-01-24 09:45:13', 1);
+INSERT INTO projects (stakeholder_id, name, description, business_man, created_by) VALUES (1, 'Our first project', 'An interesting project', 'Random name', 1);
+INSERT INTO projects (stakeholder_id, name, description, business_man, created_by) VALUES (2, 'Another great project', 'Detailed description', 'Businessman name', 1);
+
+INSERT INTO documents (document_type, stakeholder_id, operation_id, related_external_document_id, status, created_at, created_by, updated_at, updated_by) VALUES ('PURCHASE_ORDER', 10, 1, 12345, 'APPROVED', '2020-07-15 09:45:13', 3, '2020-07-15 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, project_id, status, created_at, created_by, updated_at, updated_by) VALUES ('SELL_PRE_INVOICE', 5, 1, 'PENDING', '2020-08-15 09:45:13', 2, '2020-08-15 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, project_id, operation_id, related_internal_document_id, status, created_at, created_by, updated_at, updated_by) VALUES ('SELL_INVOICE', 5, 1, 2, 2, 'APPROVED', '2020-09-01 09:45:13', 1, '2020-09-01 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, project_id, status, start_date, end_date, created_at, created_by, updated_at, updated_by) VALUES ('RENT_PRE_INVOICE', 6, 2, 'PENDING', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-20 09:45:13', 2, '2021-01-24 09:45:13', 1);
+INSERT INTO documents (document_type, stakeholder_id, project_id, operation_id, related_internal_document_id, status, start_date, end_date, created_at, created_by, updated_at, updated_by) VALUES ('RENT_INVOICE', 6, 2, 3, 4, 'APPROVED', '2021-01-25 09:45:13', '2021-02-25 09:45:13', '2021-01-24 09:45:13', 1, '2021-01-24 09:45:13', 1);
 
 UPDATE documents SET status = 'APPROVED', operation_id = 2, related_internal_document_id = 2 WHERE id = 3;
 UPDATE documents SET status = 'APPROVED', operation_id = 3, related_internal_document_id = 4 WHERE id = 5;
