e62d3d4aabec3471174e3087c7d37f3ff5b6ba29
rzlams <lamsarmiento@gmail.com>
2021-06-06 23:08:39 -0400

    add projects

---
+++ services/projects/storage.js
@@ -0,0 +1,33 @@
+const { getWhereConditions } = require(`${process.env['FILE_ENVIRONMENT']}/layers/lib`)
+
+const findAllBy = (fields = {}) => `
+  SELECT
+    id,
+    stakeholder_id,
+    status,
+    name,
+    description,
+    start_date,
+    end_date,
+    business_man,
+    created_at,
+    created_by,
+    updated_at,
+    updated_by
+  FROM projects
+  ${getWhereConditions({ fields, hasPreviousConditions: false })}
+`
+
+const findOptionsBy = (fields = {}) => `
+  SELECT id, name
+  FROM projects
+  ${getWhereConditions({ fields, hasPreviousConditions: false })}
+`
+
+const findProjectsStatus = () => `DESCRIBE projects status`
+
+module.exports = {
+  findAllBy,
+  findOptionsBy,
+  findProjectsStatus,
+}
