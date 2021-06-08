efe70738a861b691181650097e48f9164f9002e2
rzlams <lamsarmiento@gmail.com>
2021-06-08 13:53:39 -0400

    update users

--- services/users/handler.js
+++ services/users/handler.js
@@ -1,9 +1,7 @@
-const mysql = require('mysql2/promise')
-const crypto = require('crypto-js')
+const { db, helpers, isEmail, ValidatorException, encrypt } = require(`${process.env['FILE_ENVIRONMENT']}/layers/lib`)
+const { handleRequest, handleResponse, handleRead } = helpers
 const AWS = require('aws-sdk')
-const { dbConfig } = require(`${process.env['FILE_ENVIRONMENT']}/globals/dbConfig`)
-const { response, getBody, getLastId, escapeFields } = require(`${process.env['FILE_ENVIRONMENT']}/globals/common`)
-const { findAllBy, createUser, updateUser } = require('./storage')
+const storage = require('./storage')
 
 AWS.config.update({
   accessKeyId: process.env['ACCESS_KEY_ID'],
@@ -13,118 +11,155 @@ AWS.config.update({
 const cognito = new AWS.CognitoIdentityServiceProvider()
 
 module.exports.read = async event => {
-  const connection = await mysql.createConnection(dbConfig)
   try {
-    const queryParams = event.queryStringParameters ? event.queryStringParameters : {}
-    const params = escapeFields(queryParams)
+    const req = await handleRequest({ event })
 
-    const [users] = await connection.execute(findAllBy(params))
+    const res = await handleRead(req, { dbQuery: db.query, storage: storage.findAllBy })
 
-    return await response(200, { message: users }, connection)
+    return await handleResponse({ req, res })
   } catch (error) {
     console.log(error)
-    return await response(400, error, connection)
+    return await handleResponse({ error })
   }
 }
 
 module.exports.create = async event => {
-  const connection = await mysql.createConnection(dbConfig)
   try {
+    const inputType = {
+      fullName: { type: 'string', required: true },
+      password: { type: 'string', length: 100, required: true },
+      email: { type: 'email', required: true },
+      rolId: { type: 'string', length: 20, required: true },
+    }
+    const req = await handleRequest({ event, inputType })
+
+    const { fullName, password, email, rolId } = req.body
+
+    const errors = []
     const requiredFields = ['fullName', 'password', 'email', 'rolId']
-    const body = escapeFields(getBody(event))
-    const errorFields = requiredFields.filter(k => !body[k])
+    const requiredErrorFields = requiredFields.filter(k => !req.body[k])
+    const [userExists] = await db.query(storage.checkExists({ email }))
 
-    if (errorFields.length > 0) return await response(400, { message: `The fields ${errorFields.join(', ')} are required` }, connection)
+    if (requiredErrorFields.length > 0) requiredErrorFields.forEach(ef => errors.push(`The field ${ef} is required`))
+    if (userExists) errors.push(`The provided email is already registered`)
+    if (email && !isEmail(email)) errors.push(`The provided email is invalid`)
 
-    const { fullName, password, email, rolId } = body
-    const [[user]] = await connection.execute(findAllBy({ email }))
+    if (errors.length > 0) throw new ValidatorException(errors)
 
-    if (user) return await response(400, { message: 'The provided email is already registered' }, connection)
+    const cipherPassword = encrypt(password, process.env['ENCRYPTION_KEY'])
 
-    const cipherPassword = crypto.AES.encrypt(password, process.env['ENCRYPTION_KEY']).toString()
-    await connection.execute(createUser(), [fullName, cipherPassword, email, rolId])
-    const [[id]] = await connection.execute(getLastId())
+    const res = await db.transaction(async connection => {
+      await connection.query(storage.createUser(), [fullName, cipherPassword, email, rolId])
+      const id = await connection.geLastInsertId()
 
-    return await response(201, { message: id }, connection)
+      return { statusCode: 201, data: { id }, message: 'User created successfully' }
+    })
+
+    return await handleResponse({ req, res })
   } catch (error) {
     console.log(error)
-    return await response(400, error, connection)
+    return await handleResponse({ error })
   }
 }
 
 module.exports.update = async event => {
-  const connection = await mysql.createConnection(dbConfig)
   try {
-    const requiredFields = ['id', 'fullName', 'password', 'email', 'rolId']
-    const body = escapeFields(getBody(event))
-    const errorFields = requiredFields.filter(k => !body[k])
+    const inputType = {
+      id: { type: ['string', 'number'], required: true },
+      fullName: { type: 'string', required: true },
+      email: { type: 'email', required: true },
+      rolId: { type: 'string', length: 20, required: true },
+    }
+    const req = await handleRequest({ event, inputType })
 
-    if (errorFields.length > 0) return await response(400, { message: `The fields ${errorFields.join(', ')} are required` }, connection)
+    const { id, fullName, email, rolId } = req.body
 
-    const { id, fullName, password, email, rolId } = body
-    const [[userExists]] = await connection.execute(findAllBy({ id }))
+    const errors = []
+    const requiredFields = ['id', 'fullName', 'email', 'rolId']
+    const requiredErrorFields = requiredFields.filter(k => !req.body[k])
+    const [userExists] = await db.query(storage.checkExists({ id }))
+    const [emailExists] = await db.query(storage.checkExists({ email }))
 
-    if (!userExists) return await response(400, { message: `The user with the id ${id} is not registered` }, connection)
+    if (requiredErrorFields.length > 0) requiredErrorFields.forEach(ef => errors.push(`The field ${ef} is required`))
+    if (!userExists) errors.push(`The user with id ${id} is not registered`)
+    if (emailExists && Number(emailExists.id) !== Number(id)) errors.push(`The provided email is already registered`)
+    if (email && !isEmail(email)) errors.push(`The provided email is invalid`)
 
-    const cipherPassword = crypto.AES.encrypt(password, process.env['ENCRYPTION_KEY']).toString()
-    await connection.execute(updateUser(), [fullName, email, cipherPassword, rolId, id])
+    if (errors.length > 0) throw new ValidatorException(errors)
 
-    return await response(200, { message: { id } }, connection)
+    const res = await db.transaction(async connection => {
+      await connection.query(storage.updateUser(), [fullName, email, rolId, id])
+
+      return { statusCode: 200, data: { id }, message: 'User updated successfully' }
+    })
+
+    return await handleResponse({ req, res })
   } catch (error) {
     console.log(error)
-    return await response(400, error, connection)
+    return await handleResponse({ error })
   }
 }
 
 module.exports.delete = async event => {
   try {
-    const { userName } = getBody(event)
-
-    let params = {
-      UserPoolId: process.env['USER_POOL_ID'],
-      Username: userName,
+    const inputType = {
+      id: { type: ['string', 'number'], required: true },
     }
+    const req = await handleRequest({ event, inputType })
 
-    const result = await new Promise((resolve, reject) => {
-      cognito.adminDeleteUser(params, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
+    const { id } = req.body
+
+    const errors = []
+    const requiredFields = ['id']
+    const requiredErrorFields = requiredFields.filter(k => !req.body[k])
+
+    if (requiredErrorFields.length > 0) requiredErrorFields.forEach(ef => errors.push(`The field ${ef} is required`))
+
+    if (errors.length > 0) throw new ValidatorException(errors)
+
+    const res = await db.transaction(async connection => {
+      await connection.query(storage.deleteUser(), [id])
+
+      return { statusCode: 200, data: { id }, message: 'User deleted successfully' }
     })
 
-    return await response(200, { message: result })
+    return await handleResponse({ req, res })
   } catch (error) {
     console.log(error)
-    return await response(400, error)
+    return await handleResponse({ error })
   }
 }
 
 module.exports.changePassword = async event => {
   try {
-    const { accessToken, previousPassword, proposedPassword } = getBody(event)
+    const inputType = {
+      accessToken: { type: ['string', 'number'], required: true },
+      previousPassword: { type: 'string', required: true },
+      proposedPassword: { type: 'string', required: true },
+    }
+    const req = await handleRequest({ event, inputType })
 
-    let params = {
+    const { accessToken, previousPassword, proposedPassword } = req.body
+
+    const params = {
       AccessToken: accessToken,
       PreviousPassword: previousPassword,
       ProposedPassword: proposedPassword,
     }
 
-    const result = await new Promise((resolve, reject) => {
+    const res = await new Promise((resolve, reject) => {
       cognito.changePassword(params, (err, data) => {
         if (err) {
           reject(err)
         } else {
-          resolve(data)
+          resolve({ statusCode: 200, data, message: 'Password updated successfully' })
         }
       })
     })
 
-    return await response(200, { message: result })
+    return await handleResponse({ req, res })
   } catch (error) {
     console.log(error)
-    return await response(400, error)
+    return await handleResponse({ error })
   }
 }
--- services/users/storage.js
+++ services/users/storage.js
@@ -1,36 +1,25 @@
-const { getWhereConditions } = require(`${process.env['FILE_ENVIRONMENT']}/globals/common`)
+const { getWhereConditions } = require(`${process.env['FILE_ENVIRONMENT']}/layers/lib`)
 
-const findAllBy = (fields = {}, debug) => {
-  const query = `
-    SELECT id, full_name, email, rol_id
-    FROM users
-    WHERE is_active = 1 ${getWhereConditions({ fields, hasPreviousConditions: true })}
-  `
-  if (debug) console.log(query)
-  return query
-}
+const findAllBy = (fields = {}, initCondition = `is_active = 1`) => `
+  SELECT id, full_name, email, rol_id
+  FROM users
+  WHERE ${initCondition} ${getWhereConditions({ fields, hasPreviousConditions: true })}
+`
 
-const createUser = debug => {
-  const query = 'INSERT INTO users (full_name, password, email, rol_id, is_active) VALUES(?, ?, ?, ?, 1)'
-  if (debug) console.log(query)
-  return query
-}
+const checkExists = (fields = {}) => `SELECT id, email FROM users ${getWhereConditions({ fields, hasPreviousConditions: false })}`
 
-const updateUser = debug => {
-  const query = 'UPDATE users SET full_name = ?, email = ?, password = ?, rol_id = ? WHERE id = ?'
-  if (debug) console.log(query)
-  return query
-}
+const createUser = () => `
+  INSERT INTO users (full_name, password, email, rol_id, is_active) VALUES(?, ?, ?, ?, 1)
+`
 
-const deleteUser = debug => {
-  const query = 'UPDATE users SET is_active = 0 WHERE id = ?'
-  if (debug) console.log(query)
-  return query
-}
+const updateUser = () => `UPDATE users SET full_name = ?, email = ?, rol_id = ? WHERE id = ?`
+
+const deleteUser = () => `UPDATE users SET is_active = 0 WHERE id = ?`
 
 module.exports = {
   createUser,
   deleteUser,
   findAllBy,
   updateUser,
+  checkExists,
 }
