4f2a9e66c5281f19f37da45514c20b16282245e8
userlab-luismoreno <69315507+userlab-luismoreno@users.noreply.github.com>
2021-06-07 23:18:20 -0400

    Sales (#15)
    * sales
    
    * add findSalesStatus
    
    Co-authored-by: rzlams <lamsarmiento@gmail.com>

---
+++ layers/lib/helpers/handleUpdateDocument.js
@@ -0,0 +1,85 @@
+// importante especificar el document_type del documento a crear
+
+// req.body: { document_type, start_date, end_date, products }
+
+const handleUpdateDocument = async (req, res) => {
+  const {
+    document_id,
+    related_external_document_id,
+    start_date,
+    end_date,
+    products,
+    old_products,
+    project_id,
+    related_internal_document_id,
+    comments,
+    received_by,
+    updated_by = 1,
+  } = req.body
+
+  await res.connection.query(updateDocument(), [
+    related_external_document_id,
+    start_date,
+    end_date,
+    project_id,
+    related_internal_document_id,
+    comments,
+    received_by,
+    updated_by,
+    document_id,
+  ])
+
+  const deleteProductIds = old_products.flatMap(op => (products.some(p => Number(p.product_id) === Number(op.product_id)) ? [] : op.product_id))
+  const updateDocumentsProductsValues = products.map(
+    p =>
+      `(
+          ${document_id},
+          ${p.product_id},
+          ${p.product_price},
+          ${p.product_quantity},
+          ${p.product_return_cost ?? null},
+          ${p.tax_fee},
+          ${p.unit_tax_amount}
+        )`
+  )
+
+  if (deleteProductIds.length > 0) await res.connection.query(deleteDocumentProducts(deleteProductIds), [document_id])
+  await res.connection.query(updateDocumentsProducts(updateDocumentsProductsValues))
+
+  return {
+    req: { ...req, body: { ...req.body, document_id, related_internal_document_id } },
+    res: { ...res, statusCode: 201, data: { document_id }, message: 'Document created successfully' },
+  }
+}
+
+const updateDocument = () => `
+  UPDATE documents
+    SET
+      related_external_document_id = ?,
+      start_date = ?,
+      end_date = ?,
+      project_id = ?,
+      related_internal_document_id = ?,
+      comments = ?,
+      received_by = ?,
+      updated_by = ?
+  WHERE id = ?
+`
+
+const deleteDocumentProducts = productIds => `
+  DELETE FROM documents_products WHERE document_id = ? AND product_id IN (${productIds.join(', ')})
+`
+
+const updateDocumentsProducts = valuesArray => `
+  INSERT INTO documents_products
+  (document_id, product_id, product_price, product_quantity, product_return_cost, tax_fee, unit_tax_amount)
+  VALUES ${valuesArray.join(', ')}
+  ON DUPLICATE KEY UPDATE
+    product_price = VALUES(product_price),
+    product_quantity = VALUES(product_quantity),
+    product_return_cost = VALUES(product_return_cost),
+    tax_fee = VALUES(tax_fee),
+    unit_tax_amount = VALUES(unit_tax_amount)
+`
+
+module.exports = handleUpdateDocument
---
+++ services/sales/storage.js
@@ -0,0 +1,197 @@
+const { types, getWhereConditions } = require(`${process.env['FILE_ENVIRONMENT']}/layers/lib`)
+
+const findAllBy = (fields = {}) => `
+  SELECT
+    d.id,
+    d.document_type,
+    d.stakeholder_id,
+    d.operation_id,
+    d.related_internal_document_id,
+    d.related_external_document_id,
+    d.status,
+    d.comments,
+    d.received_by,
+    d.start_date,
+    d.end_date,
+    d.cancel_reason,
+    u.full_name AS creator_name,
+    d.created_at,
+    d.created_by,
+    d.updated_at,
+    d.updated_by,
+    (CASE
+      WHEN 
+        (d.related_internal_document_id IS NOT NULL AND d.operation_id IS NOT NULL) OR
+        d.status = '${types.documentsStatus.CANCELLED}'
+      THEN 1
+      ELSE 0
+    END) AS has_related_invoice,
+    s.id AS stakeholder_id,
+    s.name AS stakeholder_name,
+    s.business_man AS stakeholder_business_man,
+    s.address AS stakeholder_address,
+    s.phone AS stakeholder_phone,
+    proj.id AS project_id,
+    proj.name AS project_name,
+    proj.business_man AS project_business_man,
+    prod.id AS products__id,
+    prod.status AS products__status,
+    dp.product_price AS products__unit_price,
+    dp.product_quantity AS products__quantity,
+    dp.product_return_cost AS products__product_return_cost,
+    prod.code AS products__code,
+    prod.serial_number AS products__serial_number,
+    prod.description AS products__description
+  FROM documents d
+  INNER JOIN users u ON u.id = d.created_by
+  INNER JOIN documents_products dp ON dp.document_id = d.id
+  INNER JOIN products prod ON prod.id = dp.product_id
+  INNER JOIN stakeholders s ON s.id = d.stakeholder_id
+  INNER JOIN projects proj ON proj.id = d.project_id
+  WHERE (
+    d.document_type = '${types.documentsTypes.SELL_PRE_INVOICE}' OR
+    d.document_type = '${types.documentsTypes.RENT_PRE_INVOICE}'
+  ) ${getWhereConditions({ fields, tableAlias: 'd' })}
+`
+
+const findSalesStatus = () => `DESCRIBE documents status`
+
+const findStakeholder = (fields = {}, initWhereCondition = `status = '${types.stakeholdersStatus.ACTIVE}'`) => `
+  SELECT id, stakeholder_type, status, name, address, nit, email, phone, alternative_phone, business_man, payments_man,block_reason, created_at, created_by, updated_at, updated_by
+  FROM stakeholders
+  WHERE ${initWhereCondition} ${getWhereConditions({ fields })}
+`
+
+const findProductReturnCost = whereIn => `
+  SELECT im.product_id, im.unit_cost AS product_return_cost
+  FROM inventory_movements im
+  WHERE im.id IN (
+    (
+      SELECT MAX(subim.id) AS id
+      FROM inventory_movements subim
+      WHERE subim.movement_type = 'IN' AND subim.status = 'APPROVED' AND subim.product_id IN (${whereIn.join(', ')})
+      GROUP BY subim.product_id
+    )
+  )
+  ORDER BY id DESC
+`
+
+const findProducts = whereIn => `
+  SELECT
+    p.id AS product_id,
+    p.stock,
+    p.unit_price AS product_price,
+    t.fee AS tax_fee
+  FROM products p
+  LEFT JOIN taxes t ON t.id = p.tax_id
+  WHERE p.id IN (${whereIn.join(', ')})
+`
+
+const findDocument = () => `
+  SELECT
+    d.id AS document_id,
+    d.document_type AS document_type,
+    d.stakeholder_id AS stakeholder_id,
+    d.operation_id AS operation_id,
+    o.operation_type AS operation_type,
+    d.project_id AS project_id,
+    d.related_internal_document_id AS related_internal_document_id,
+    d.related_external_document_id AS related_external_document_id,
+    d.status AS status,
+    d.comments AS comments,
+    d.received_by AS received_by,
+    d.start_date AS start_date,
+    d.end_date AS end_date,
+    d.cancel_reason AS cancel_reason,
+    d.created_at AS created_at,
+    d.created_by AS created_by,
+    d.updated_at AS updated_at,
+    d.updated_by AS updated_by,
+    im.id AS old_inventory_movements__inventory_movement_id,
+    im.operation_id AS old_inventory_movements__operation_id,
+    im.product_id AS old_inventory_movements__product_id,
+    im.quantity AS old_inventory_movements__quantity,
+    im.unit_cost AS old_inventory_movements__unit_cost,
+    im.movement_type AS old_inventory_movements__movement_type,
+    im.status AS old_inventory_movements__status,
+    p.stock AS old_inventory_movements__stock,
+    dp.product_id AS old_products__product_id,
+    dp.product_price AS old_products__product_price,
+    dp.product_quantity AS old_products__product_quantity,
+    dp.product_return_cost AS old_products__product_return_cost,
+    dp.tax_fee AS old_products__tax_fee,
+    dp.unit_tax_amount AS old_products__unit_tax_amount,
+    p.stock AS old_products__stock
+  FROM documents d
+  LEFT JOIN documents_products dp ON dp.document_id = d.id
+  LEFT JOIN products p ON p.id = dp.product_id
+  LEFT JOIN operations o ON o.id = d.operation_id
+  LEFT JOIN inventory_movements im ON im.operation_id = o.id
+  WHERE d.id = ? AND (
+    d.document_type = '${types.documentsTypes.SELL_PRE_INVOICE}' OR
+    d.document_type = '${types.documentsTypes.RENT_PRE_INVOICE}'
+  )
+`
+
+const findDocumentDetails = () => `
+  SELECT
+    d.id AS document_id,
+    d.related_internal_document_id AS related_internal_document_id,
+    d.document_type AS document_type,
+    d.status AS document_status,
+    d.operation_id AS operation_id,
+    d.stakeholder_id AS stakeholder_id,
+    d.project_id AS project_id,
+    o.operation_type AS operation_type,
+    dp.product_id AS products__product_id,
+    dp.product_quantity AS products__product_quantity,
+    dp.product_price AS products__product_price,
+    COALESCE(dp.product_return_cost, dp.product_price) AS products__product_return_cost,
+    dp.tax_fee AS products__tax_fee,
+    dp.unit_tax_amount AS products__unit_tax_amount,
+    p.status AS products__product_status
+  FROM documents d
+  LEFT JOIN documents_products dp ON dp.document_id = d.id
+  LEFT JOIN products p ON p.id = dp.product_id
+  LEFT JOIN operations o ON o.id = d.operation_id
+  WHERE 
+  d.id = ? AND (
+    d.document_type = '${types.documentsTypes.SELL_PRE_INVOICE}' OR
+    d.document_type = '${types.documentsTypes.RENT_PRE_INVOICE}'
+  )
+`
+
+const findDocumentMovements = () => `
+  SELECT
+    d.id AS document_id,
+    d.related_internal_document_id AS related_internal_document_id,
+    d.document_type AS document_type,
+    d.status AS document_status,
+    d.operation_id AS operation_id,
+    im.id AS inventory_movements__inventory_movement_id,
+    im.movement_type AS inventory_movements__movement_type,
+    im.product_id AS inventory_movements__product_id,
+    im.quantity AS inventory_movements__quantity,
+    p.stock AS inventory_movements__stock,
+    p.status AS inventory_movements__product_status
+  FROM documents d
+  LEFT JOIN operations o ON o.id = d.operation_id
+  LEFT JOIN inventory_movements im ON im.operation_id = o.id
+  LEFT JOIN products p ON p.id = im.product_id
+  WHERE
+    d.id = ? AND (
+      d.document_type = '${types.documentsTypes.SELL_PRE_INVOICE}' OR
+      d.document_type = '${types.documentsTypes.RENT_PRE_INVOICE}'
+    )
+`
+
+module.exports = {
+  findAllBy,
+  findDocument,
+  findDocumentDetails,
+  findDocumentMovements,
+  findProducts,
+  findProductReturnCost,
+  findSalesStatus,
+  findStakeholder,
+}
