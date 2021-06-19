ed8c1738e03a6b418b2c73c98d244d828f61dfcd
rzlams <lamsarmiento@gmail.com>
2021-06-19 18:53:34 -0400

    update permissions and update password

--- services/users/storage.js
+++ services/users/storage.js
@@ -7,17 +7,30 @@ const findAllBy = (fields = {}, initCondition = `u.is_active = 1`) => `
     u.email,
     u.rol_id,
     r.name AS rol_name,
-    r.permissions AS permissions
+    u.permissions AS permissions
   FROM users u
   INNER JOIN roles r ON r.id = u.rol_id
-  WHERE ${initCondition} ${getWhereConditions({ fields, tableAlias: 'u', hasPreviousConditions: true })}
+  WHERE ${initCondition} ${getWhereConditions({ fields, tableAlias: 'u' })}
 `
 
-const checkExists = (fields = {}) => `SELECT id, email FROM users ${getWhereConditions({ fields, hasPreviousConditions: false })}`
+const findRoles = (fields = {}, initCondition = `is_active = 1`) => `
+  SELECT id, name
+  FROM roles
+  WHERE ${initCondition} ${getWhereConditions({ fields })}
+`
+
+const checkExists = (fields = {}) => `SELECT id, email, password FROM users ${getWhereConditions({ fields, hasPreviousConditions: false })}`
 
 const createUser = () => `
-  INSERT INTO users (full_name, password, email, rol_id, is_active) VALUES(?, ?, ?, ?, 1)
+  INSERT INTO users (full_name, password, email, rol_id, is_active, permissions)
+  VALUES(?, ?, ?, ?, 1, (SELECT permissions FROM roles WHERE id = ?))
 `
+
+const updatePermissions = () => `UPDATE users SET permissions = ? WHERE id = ?`
+
+const findPassword = () => `SELECT password FROM users WHERE id = ?`
+
+const updatePassword = () => `UPDATE users SET password = ? WHERE id = ?`
 
 const updateUser = () => `UPDATE users SET full_name = ?, email = ?, rol_id = ? WHERE id = ?`
 
@@ -27,6 +40,10 @@ module.exports = {
   createUser,
   deleteUser,
   findAllBy,
+  findPassword,
+  findRoles,
+  updatePassword,
+  updatePermissions,
   updateUser,
   checkExists,
 }
