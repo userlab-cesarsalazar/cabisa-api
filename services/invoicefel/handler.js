7bc2c60fc7270794cfdf4f6b804176b6bfe84bed
userlab-alejandrorodriguez <87735711+userlab-alejandrorodriguez@users.noreply.github.com>
2022-06-29 11:26:14 -0600

    The create handler for the invoicesCreate service has been added

--- services/invoicefel/handler.js
+++ services/invoicefel/handler.js
@@ -1,17 +1,36 @@
-
-const mysql = require('mysql2/promise')
-const storage = require('./storage')
-const { handleRequest, handleResponse, handleRead } = helpers
-const db = mysqlConfig(mysql)
+//const mysql = require('mysql2/promise')
+//const storage = require('./storage')
+//const db = mysqlConfig(mysql)
+const axios = require('../../layers/nodejs/node_modules/axios')
+const moment = require('../../layers/nodejs/node_modules/moment-timezone')
+const helpers = require('../../globals/helpers')
+const { buildXml, handleRequest, handleResponse, handleRead } = helpers
 
 module.exports.create = async (event, context) => {
-    try {
-        
-        const req = await handleRequest({ event })        
-        return await handleResponse({ message:'hola mundo' })
+  try {
+    const { body } = await handleRequest({ event })
 
-    } catch (error) {
-        console.log("invoice Errors",error)
-        return await handleResponse({ error })
-    }
+    if (!body) throw new Error('Missing body')
+
+    const xml = buildXml(body, moment)
+
+    const response = await axios.post(process.env.CERTIFIER_URL, xml, {
+      headers: {
+        UsuarioFirma: process.env.SIGNATURE_USER_SAT,
+        LlaveFirma: process.env.SIGNATURE_KEY_SAT,
+        UsuarioApi: process.env.API_USER_SAT,
+        LlaveApi: process.env.API_KEY_SAT,
+        identificador: process.env.IDENTIFIER_SAT,
+      },
+    })
+
+    if (!response && !response.data) throw new Error('The request to the SAT certification service has failed.')
+
+    const res = { statusCode: 200, data: response.data, message: 'Successful response' }
+
+    return await handleResponse({ req: {}, res })
+  } catch (error) {
+    console.log('invoicefel Errors', error)
+    return await handleResponse({ error })
+  }
 }
