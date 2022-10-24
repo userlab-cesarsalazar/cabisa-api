0dfe73f71cabf5565d5dd097eaf3f27029237da2
userlab-luisdeleon <59143067+userlab-luisdeleon@users.noreply.github.com>
2022-10-24 00:37:48 -0600

    Update buildCancelXml.js

--- globals/helpers/buildCancelXml.js
+++ globals/helpers/buildCancelXml.js
@@ -6,7 +6,7 @@ const cancelXml = (data, moment) => {
     <dte:GTAnulacionDocumento xmlns:dte="http://www.sat.gob.gt/dte/fel/0.1.0" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:n1="http://www.altova.com/samplexml/other-namespace" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" Version="0.1" xsi:schemaLocation="http://www.sat.gob.gt/dte/fel/0.1.0 C:\Users\User\Desktop\FEL\Esquemas\GT_AnulacionDocumento-0.1.0.xsd">
         <dte:SAT>
             <dte:AnulacionDTE ID="DatosCertificados">
-                <dte:DatosGenerales FechaEmisionDocumentoAnular="${data.certificateDate}" FechaHoraAnulacion="${moment().tz('America/Guatemala').format()}" ID="DatosAnulacion" IDReceptor="${data.nit}" MotivoAnulacion="${data.description}" NITEmisor="${nitDev}" NumeroDocumentoAAnular="${data.uuid}" />          
+                <dte:DatosGenerales FechaEmisionDocumentoAnular="${data.certificateDate}" FechaHoraAnulacion="${moment().tz('America/Guatemala').format()}" ID="DatosAnulacion" IDReceptor="${data.nit}" MotivoAnulacion="${data.description}" NITEmisor="${nitProd}" NumeroDocumentoAAnular="${data.uuid}" />          
             </dte:AnulacionDTE>
         </dte:SAT>
     </dte:GTAnulacionDocumento>`
